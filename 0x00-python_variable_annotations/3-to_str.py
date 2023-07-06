0e64718f15f081c55b63d613e42ba1ae7402d70e
Hammed Tosin <hammedtosin188@gmail.com>
2023-07-06 17:21:37 +0000

    convert float to str and test

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+a function that takes a float argument
+and returns a string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    take float and return str
+    """
+    return str(n)

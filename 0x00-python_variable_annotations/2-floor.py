e18a854cd9796070a67973a15a0e8dcefb7f8b40
Hammed Tosin <hammedtosin188@gmail.com>
2023-07-06 17:12:21 +0000

    use a NewType method to create aliase

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
 import math
+from typing import NewType
 
 """
 a function that take n (float) as a parameter
 and return floor of it
 """
+floor = NewType("floor", int)
 
 
-def floor(n: float) -> int:
+def floor(n: float) -> floor:
     """
     takes n(float) as arg and return int
     """

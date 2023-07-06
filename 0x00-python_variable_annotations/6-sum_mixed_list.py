fe876073546e2044102c91a1ca7c2d819876bb2e
Hammed Tosin <hammedtosin188@gmail.com>
2023-07-06 20:00:53 +0000

    mixed list param

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import typing
+"""
+a function that takes a list of int an and float
+"""
+Mxd_lst = typing.List[float]
+
+
+def sum_mixed_list(mxd_lst: Mxd_lst) -> float:
+    """
+    take mixed type of value as parameter and return
+    float
+    """
+    return sum(mxd_lst)

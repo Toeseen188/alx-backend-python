3210d0a04e4d7fdc7e6596060e93152cf74c1326
Hammed Tosin <hammedtosin188@gmail.com>
2023-07-06 16:25:25 +0000

    using var annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+create a function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    takes two arguments, both are floats
+    and also return float
+    """
+    return a + b

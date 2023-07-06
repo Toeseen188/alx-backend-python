3b4ff51a9416b226a08ddf451d25e7734abf8811
Hammed Tosin <hammedtosin188@gmail.com>
2023-07-06 16:45:01 +0000

    concatenate 2 string value

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+a function that takes two strings parameters
+and concatenate them together
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    this function concatenate two
+    string value
+    """
+    return str1 + str2

ec2b14c2f58617aed97ee952f06d92c3f553e294
Hammed Tosin <hammedtosin188@gmail.com>
2023-07-06 17:04:09 +0000

    importing math.floor() lib

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import math
+"""
+a function that take n (float) as a parameter
+and return floor of it
+"""
+
+
+def floor(n: float) -> int:
+    """
+    takes n(float) as arg and return int
+    """
+    return math.floor(n)

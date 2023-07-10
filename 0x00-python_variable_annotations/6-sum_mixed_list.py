6886ad1a831758337c044a258af0ee9018edf489
Hammed Tosin <hammedtosin188@gmail.com>
2023-07-10 14:32:05 +0000

    put declaration and import before comment

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import typing
+Mxd_lst = typing.List[typing.Union[int, float]]
 """
 a function that takes a list of int an and float
 """
-Mxd_lst = typing.List[typing.Union[int, float]]
 
 
 def sum_mixed_list(mxd_lst: Mxd_lst) -> float:

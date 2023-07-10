4deef113d23e185122c0ca158b3e39ecb2a52fad
Hammed Tosin <hammedtosin188@gmail.com>
2023-07-10 14:22:55 +0000

    using typing.Union for alternative types and typing.Tuple for tuple type

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import typing
+"""
+this module contain a function that
+takes a str 'k' and int/float 'v' as arguments
+and return a tuple
+"""
+
+Tuple_ = typing.Tuple[str, float]
+
+
+def to_kv(k: str, v: typing.Union[int, float]) -> Tuple_:
+    """
+    this function takes a string k and int or float k
+    and return a tuple(int, float)
+    """
+    return (k, v*v)

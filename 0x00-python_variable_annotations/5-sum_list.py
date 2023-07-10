68e4f57a9a1825bf029c9d57fb07dd0de1bbec3c
Hammed Tosin <hammedtosin188@gmail.com>
2023-07-10 14:41:46 +0000

    placed import below comment

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from typing import List
 """
 a function that accept a list of float
 and return the sum
 """
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:

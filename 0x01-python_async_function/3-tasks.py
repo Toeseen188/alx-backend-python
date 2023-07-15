878d4b22d17eea1fe6e9af2f80721d91500219e5
Hammed Tosin <hammedtosin188@gmail.com>
2023-07-15 14:59:13 +0000

    used asyncio.Task insted of asycio.create_task()

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,12 +7,13 @@ use the regular function syntax to do this) task_wait_random
 that takes an integer max_delay and returns a asyncio.Task
 """
 import asyncio
+import typing
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> float:
     """
-    a  normal func that take an int and
+    a normal func that take an int and
     returns an asyncio.Task
     """
-    return asyncio.create_task(wait_random(max_delay))
+    return asyncio.Task(wait_random(max_delay))
